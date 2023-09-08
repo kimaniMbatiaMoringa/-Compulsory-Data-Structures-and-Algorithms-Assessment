08d4542d056b147840095e05453112980f86ef49
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-09-09 01:39:34 +0300

    function3 implemented

--- main.py
+++ main.py
@@ -33,4 +33,20 @@ def remove_duplicates(sequence):
         
 sequence1 = [2, 3, 2, 4, 5, 3, 6, 7, 5]
 
+
+def word_frequency(sentence):
+    words = []
+    dic = {}
+    spliting = sentence.split()
+    for item in spliting:
+        x = 1
+        if item in words:
+           x+= 1
+        words.append(item)
+        dic[item]= x
+    
+    return dic
+
+uno = "my my love is good"
+
 ipdb.set_trace()
